e7add8c05cee504a057c392bd80fb6e9d89a545e
Chemmy <chengming0916@outlook.com>
2023-07-14 16:13:53 +0800

    TcpClientSample初版

--- TcpClientSample/mainwindow.cpp
+++ TcpClientSample/mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
+MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    tcpClient = new QTcpSocket(this);
+    connect(tcpClient, &QTcpSocket::readyRead, this, &MainWindow::tcp_client_readdata);
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +15,39 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_btn_connect_clicked()
+{
+    if (tcpClient->state() != QAbstractSocket::ConnectedState) {
+        QString ip = ui->txt_ip->text();
+        ushort port = ui->txt_port->text().toUShort();
+        tcpClient->connectToHost(ip, port);
+
+        // 等待连接成功
+        if (!tcpClient->waitForConnected(30000)) {
+            qDebug() << "Connection failed";
+            return;
+        }
+
+        ui->btn_connect->setText("断开");
+    } else {
+        tcpClient->disconnectFromHost();
+        ui->btn_connect->setText("连接");
+    }
+}
+
+void MainWindow::on_btn_send_clicked()
+{
+    if (tcpClient->state() == QAbstractSocket::ConnectedState) {
+        QString send_str = ui->txt_input->toPlainText();
+        tcpClient->write(send_str.toLocal8Bit());
+        ui->txt_log->append("发送数据: " + send_str);
+    }
+}
+
+void MainWindow::tcp_client_readdata()
+{
+    QByteArray buffer = tcpClient->readAll();
+    QString recv_str = QString::fromLocal8Bit(buffer);
+    ui->txt_log->append("接收数据: " + recv_str);
+    qDebug() << recv_str;
+}
--- TcpClientSample/mainwindow.h
+++ TcpClientSample/mainwindow.h
@@ -2,20 +2,30 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTcpSocket>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+class MainWindow;
+}
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_btn_connect_clicked();
+
+    void on_btn_send_clicked();
+
+    void tcp_client_readdata();
+
 private:
-    Ui::MainWindow *ui;
+    Ui::MainWindow* ui;
+    QTcpSocket* tcpClient;
 };
 #endif // MAINWINDOW_H
